adccdd8cfb6c620ea3b88ca585780fb7663408a0
pedro <pedro@macboom>
2021-01-17 17:27:04 +0100

    +created view for creating a new land temperature record, + tests for that view

--- ws_data/land_temperatures/tests.py
+++ ws_data/land_temperatures/tests.py
@@ -4,11 +4,13 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from .models import LandTemperature
+
 
 class LandTemperatureTestsWithFixtures(APITestCase):
     fixtures = ['land_temperatures.json']
 
-    def test_view_land_temperatures_list(self):
+    def test_land_temperatures_list(self):
         """
         Ensure we can access a list of land_temperatures when DB has 3 entries (from fixtures)
         """
@@ -16,6 +18,14 @@ class LandTemperatureTestsWithFixtures(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 3)
+
+    def test_land_temperatures_add(self):
+        url = reverse('land_temperatures-list')
+        data = {'date': "1805-04-01", 'city_name': "Racoon City", 'country_name': "USA", 'avg_temp': -13.0,
+                'temp_uncertainty': 4.063, 'latitude': "1N", 'longitude': "2L"}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(LandTemperature.objects.count(), 4)
 
 
 class LandTemperatureTestsWithoutFixtures(APITestCase):
@@ -27,3 +37,12 @@ class LandTemperatureTestsWithoutFixtures(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 0)
+
+    def test_land_temperatures_add(self):
+        url = reverse('land_temperatures-list')
+        data = {'date': "1805-04-01", 'city_name': "Racoon City", 'country_name': "USA", 'avg_temp': -13.0,
+                'temp_uncertainty': 4.063, 'latitude': "1N", 'longitude': "2L"}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(LandTemperature.objects.count(), 1)
+        self.assertEqual(LandTemperature.objects.get().city_name, 'Racoon City')
--- ws_data/land_temperatures/views.py
+++ ws_data/land_temperatures/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import LandTemperature
 from .serializers import LandTemperatureSerializer
@@ -15,3 +16,27 @@ def land_temperature_list(request):
         temperatures = LandTemperature.objects.all()[:10]
         serializer = LandTemperatureSerializer(temperatures, many=True)
         return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = LandTemperatureSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class LandTemperaturesList(APIView):
+#     """
+#     List all snippets, or create a new snippet.
+#     """
+#     def get(self, request, format=None):
+#         temperatures = LandTemperature.objects.all()
+#         serializer = LandTemperatureSerializer(temperatures, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = LandTemperatureSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
