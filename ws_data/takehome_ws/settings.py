823b4f7c410677500ae08ab5a8313d7993dc7c39
pedro <pedro@macboom>
2021-01-17 10:51:31 +0100

    + replaced sqlite by postgres as django database

--- ws_data/takehome_ws/settings.py
+++ ws_data/takehome_ws/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'takehome_ws.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'takehome_ws_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'database',
+        'PORT': 5432,
     }
 }
 
